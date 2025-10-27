e56ba2ab19c06c0031a985742bce438cce52216f
Petr Fifka <petr.fifka@tredgate.cz>
2025-10-27 17:27:46 +0100

    add first js files: first scripts, values

---
+++ javascript/values-and-variables/values.js
@@ -0,0 +1,11 @@
+// values.js
+// values-and-variables
+console.log("String");
+console.log(15); // * number (číslo)
+console.log(10.01); // ! Funguje pouze desetinná tečka!
+console.log("25"); // ! Není number, ale string
+console.log("12" + "24"); // ? Výsledek: 1224
+console.log(true); // * boolean (logická hodnota)
+console.log(false);
+console.log(null); // * Prázdná hodnota
+console.log(undefined); // * Prázdná hodnota
